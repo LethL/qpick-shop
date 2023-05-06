f648378fc2a099f644f8450d67c31d05d217e4de
Leth4L <carribean_08@mail.ru>
2023-05-06 11:10:37 +0300

    feat: add footer

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Header from "../Header/Header";
 import GoodsList from "../GoodsList/GoodsList";
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
     <div className="app">
       <Header />
       <GoodsList />
+      <Footer />
     </div>
   );
 }
